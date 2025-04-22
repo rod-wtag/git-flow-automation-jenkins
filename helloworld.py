3ccc7c8c76c848c2aec32a5edb8c0089dc5f3464
rokydas-wd <roky.das@welldev.io>
2025-04-22 10:59:33 +0600

    release branch 21.26 initiated

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,41 @@
-hello world
-hello world 2
-hello world 3
-hello world 4
-hello world 5
+pipeline {
+    agent any
+
+    triggers {
+        githubPush()
+    }
+
+    environment {
+        BRANCH_NAME = "release/21.26"
+        TAG_NAME = "r21.26.27"
+        GIT_CREDENTIALS_ID = 'git-username'
+    }
+
+    stages {
+        stage('Only on release/21.26') {
+            when {
+                branch 'release/21.26'
+            }
+            steps {
+                echo "Triggered on release/21.26"
+            }
+        }
+
+        stage('Create & Push Tag') {
+            when {
+                branch 'release/21.26'
+            }
+            steps {
+                script {
+                    sh """
+                        git config user.name "rod-wtag"
+                        git config user.email "roky.das@welldev.io"
+
+                        git tag ${TAG_NAME}
+                        git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/your-username/your-repo.git ${TAG_NAME}
+                    """
+                }
+            }
+        }
+    }
+}
